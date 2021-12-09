3f33fac8132a78db5cd5bad81bf7a2e53ae691e8
Zhan <yi.zhan@ucdconnect.ie>
2021-12-09 22:15:29 +0800

    final

--- app/src/main/java/com/example/timecapsule/fragment/NoticeFragment.java
+++ app/src/main/java/com/example/timecapsule/fragment/NoticeFragment.java
@@ -1,6 +1,7 @@
 package com.example.timecapsule.fragment;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -26,16 +27,23 @@ import com.example.timecapsule.db.Event;
 import com.example.timecapsule.db.User;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.listener.ChartTouchListener;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
@@ -43,6 +51,7 @@ import com.github.mikephil.charting.utils.MPPointF;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -50,15 +59,19 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
 import android.app.DatePickerDialog;
 
+
+//REFERENCE: LineChart and PieChart: https://github.com/PhilJay/MPAndroidChart
 public class NoticeFragment extends Fragment {
 
     private PieChart mChart;
+    private LineChart mChart1;
     private List<Event> eventList = new ArrayList<>();
     private ImageButton calendar;
     private static Calendar cal = Calendar.getInstance();
@@ -71,6 +84,7 @@ public class NoticeFragment extends Fragment {
     private TextView textSkill;
     private String[] months = {"Jan", "Feb", "Mar", "Apr","May", "Jun", "Jul", "Aug", "Sept","Oct","Nov","Dec"};
     private float[] capsules = new float[3];
+    private int[][] week = new int[7][3];
     private TextView selYear;
     private TextView selMonth;
     private TextView selDay;
@@ -78,8 +92,10 @@ public class NoticeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         View root = inflater.inflate(R.layout.fragment_notice, container, false);
         mChart = root.findViewById(R.id.chart);
+        mChart1 = root.findViewById(R.id.chart1);
         calendar = root.findViewById(R.id.cal);
         textYear = root.findViewById(R.id.data_year);
         textMD = root.findViewById(R.id.data_month_day);
@@ -103,7 +119,12 @@ public class NoticeFragment extends Fragment {
         textBoss.setText(capsules[0]+"");
         textEnergy.setText(capsules[1]+"");
         textSkill.setText(capsules[2]+"");
+
         showPieChart(mChart, getPieChartData(capsules));
+
+        week = getWeekDate(date);
+        setLineChartData(week,eventList,mChart1);
+        setLineType(mChart1);
 
         calendar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -170,11 +191,6 @@ public class NoticeFragment extends Fragment {
             }
         });
 
-
-
-
-
-
         return root;
     }
 
@@ -234,7 +250,7 @@ public class NoticeFragment extends Fragment {
     }
 
 
-    //添加数据
+
     private static List<PieEntry> getPieChartData(float[] capsules) {
 
         List<PieEntry> mPie = new ArrayList<>();
@@ -286,56 +302,32 @@ public class NoticeFragment extends Fragment {
         dataSet.setColors(colors);
         PieData pieData = new PieData(dataSet);
 
-        // 设置描述，我设置了不显示，因为不好看，你也可以试试让它显示，真的不好看
         Description description = new Description();
         description.setEnabled(false);
         pieChart.setDescription(description);
-
-        //设置半透明圆环的半径, 0为透明
         pieChart.setTransparentCircleRadius(0.5f);
-
-        //设置初始旋转角度
         pieChart.setRotationAngle(-15);
-
-        //数据连接线距图形片内部边界的距离，为百分数
         dataSet.setValueLinePart1OffsetPercentage(100f);
-
-        //设置连接线的颜色
         dataSet.setValueLineColor(Color.BLACK);
-        // 连接线在饼状图外面
         dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-
-        // 设置饼块之间的间隔
         dataSet.setSliceSpace(5f);
         dataSet.setHighlightEnabled(true);
-        // 不显示图例
         Legend legend = pieChart.getLegend();
         legend.setEnabled(false);
-
-        // 和四周相隔一段距离,显示数据
         pieChart.setExtraOffsets(26, 5, 26, 5);
-
-        // 设置pieChart图表是否可以手动旋转
         pieChart.setRotationEnabled(true);
-        // 设置piecahrt图表点击Item高亮是否可用
         pieChart.setHighlightPerTapEnabled(true);
-        // 设置pieChart图表展示动画效果，动画运行1.4秒结束
         pieChart.animateY(1000, Easing.EaseInOutQuad);
-        //设置pieChart是否只显示饼图上百分比不显示文字
         pieChart.setDrawEntryLabels(true);
         pieChart.setEntryLabelColor(Color.rgb(100,100,100));
         pieChart.setEntryLabelTextSize(15f);
-        //是否绘制PieChart内部中心文本
         pieChart.setDrawCenterText(true);
-        // 绘制内容value，设置字体颜色大小
         pieData.setDrawValues(true);
         pieChart.setUsePercentValues(true);
         pieData.setValueFormatter(new PercentFormatter());
         pieData.setValueTextSize(18f);
         pieData.setValueTextColor(Color.BLACK);
-
         pieChart.setData(pieData);
-        // 更新 piechart 视图
         pieChart.postInvalidate();
     }
 
@@ -347,7 +339,6 @@ public class NoticeFragment extends Fragment {
         }
         return 0;
     }
-
 
 
     public static class DatePickerFragment extends androidx.appcompat.app.AppCompatDialogFragment implements DatePickerDialog.OnDateSetListener {
@@ -382,6 +373,7 @@ public class NoticeFragment extends Fragment {
             TextView textSkill = (TextView) getActivity().findViewById(R.id.skill);
 
             PieChart mChart = (PieChart) getActivity().findViewById(R.id.chart);
+            LineChart mChart1 = (LineChart) getActivity().findViewById(R.id.chart1);
 
             textviewdatemd.setText(months[month]+ " "+day);
             textviewdatey.setText(year+"");
@@ -389,6 +381,10 @@ public class NoticeFragment extends Fragment {
             String date = year + "-" + mon + "-" + day;
             float[] capsules = getDayEventList(eventList,date, type);
             showPieChart(mChart, getPieChartData(capsules));
+
+            int [][] week = getWeekDate(date);
+            setLineChartData(week,eventList,mChart1);
+            setLineType(mChart1);
 
             textBoss.setText(capsules[0]+"");
             textEnergy.setText(capsules[1]+"");
@@ -417,8 +413,139 @@ public class NoticeFragment extends Fragment {
 
     }
 
+    public static long getCalDate(String strDate) {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDF = new SimpleDateFormat("yyyy-MM-dd");
+        Date result;
+        try {
+            result = simpleDF.parse(strDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            result = null;
+        }
+        calendar.setTime(result);
+        return calendar.getTimeInMillis();
+    }
+
+    public static int[][] getWeekDate(String strDate){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        int[][] week = new int[7][3];
+        long theDay = getCalDate(strDate);
+        Calendar cal = Calendar.getInstance();
+        Date date = new Date(theDay);
+        String theDate = sdf.format(date);
+        cal.setTime(date);
+
+        for(int j =0; j < 3; j++){
+            week[0][j] = Integer.parseInt(theDate.split("-")[j]);
+        }
+        for(int i =1; i < 7; i++){
+            cal.add(Calendar.DAY_OF_YEAR, -1);
+            Date date_offset = cal.getTime();
+            String beforeDay = sdf.format(date_offset);
+            String[] strings = beforeDay.split("-");
+            for(int j =0; j < 3; j++){
+                week[i][j] = Integer.parseInt(strings[j]);
+            }
+        }
+
+        return week;
+    }
+
+    public static void setLineChartData(int[][] week, List<Event> eventList, LineChart lineChart){
+        String[] XDate = new String[7];
+        int[] YBoss = new int[7];
+        int[] YEnergy = new int[7];
+        int[] YSkill = new int[7];
+
+        for (int i = 6; i > -1; i--){
+            int[] date = week[i];
+            String SDate = (date[0]+"").substring(2,4) +"-"+ date[1] + "-" + date[2];
+            XDate[6-i] = SDate;
+            int[] capsules = new int[3];
+
+            for(Event event : eventList){
+                if(event.getYear()==date[0]&& event.getMonth()==date[1] && event.getDay()==date[2]){
+                    if(!event.isIs_complete()){
+                        if(event.getType().equals("Boss Capsule")){
+                            capsules[0] = capsules[0] + 1;
+                        }else{
+                            capsules[2] = capsules[2] + 1;
+                        }
+                    }else {
+                        capsules[1] = capsules[1] + 1;
+                    }
+                }
+            }
+
+            YBoss[6-i] = capsules[0];
+            YEnergy[6-i] = capsules[1];
+            YSkill[6-i] = capsules[2];
 
 
+        }
+
+        LineData data = new LineData(setLine(YBoss,1,"Boss"));
+        data.addDataSet(setLine(YEnergy,2,"Energy"));
+        data.addDataSet(setLine(YSkill,3,"Skill"));
+        lineChart.setData(data);
+
+        ValueFormatter valueFormatter = new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return XDate[(int) value];
+            }
+        };
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setValueFormatter(valueFormatter);
+    }
+
+
+    public static LineDataSet setLine(int[] YCapsule, int flag, String name) {
+
+
+        ArrayList<Entry> YValues = new ArrayList<Entry>();
+        for (int i = 0; i < YCapsule.length; i++) {
+            YValues.add(new Entry(i,YCapsule[i]));
+        }
+
+        LineDataSet setData = new LineDataSet(YValues,name);
+        setData.setCubicIntensity(0.2f);
+        setData.setDrawFilled(false);
+        setData.setDrawCircles(true);
+        setData.setLineWidth(2f);
+        setData.setHighLightColor(Color.rgb(244, 117, 117));
+        if(flag==1)
+            setData.setColor(Color.rgb(104, 241, 175));
+        else if(flag==2)
+            setData.setColor(Color.rgb(255, 0, 0));
+        else if(flag==3)
+            setData.setColor(Color.rgb(255, 200, 0));
+        return setData;
+    }
+
+    public static void setLineType(LineChart mChart){
+
+        mChart.setDrawBorders(false);
+        mChart.setBorderColor(Color.RED);
+        mChart.setBorderWidth(1f);
+        mChart.setTouchEnabled(true);
+        mChart.setDragDecelerationFrictionCoef(0.9f);
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDoubleTapToZoomEnabled(false);
+        mChart.setDrawGridBackground(false);
+        mChart.setHighlightPerDragEnabled(true);
+        mChart.setPinchZoom(true);
+        mChart.setBackgroundColor(Color.argb(5,3,169,233));
+        mChart.animateX(800);
+        Description description = new Description();
+        description.setText("");
+        mChart.setDescription(description);
+        mChart.setNoDataText("None Data");
+
+
+    }
 
 
 
